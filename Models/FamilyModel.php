dd402ded7b6e2e9adc7ca26219c705428ea7d3c7
Haryo Kuncoro <61614504+haryo-sk@users.noreply.github.com>
2022-07-05 16:32:16 +0700

    Create FamilyModel.php

---
+++ Models/FamilyModel.php
@@ -0,0 +1,12 @@
+<?php
+ 
+namespace App\Models;
+ 
+use CodeIgniter\Model;
+ 
+class FamilyModel extends Model
+{
+    protected $table = 'ci_data_keluarga';
+    protected $primaryKey = 'ID';
+    protected $allowedFields = ['NAMA_LENGKAP','TGL_LAHIR','STATUS'];
+}
