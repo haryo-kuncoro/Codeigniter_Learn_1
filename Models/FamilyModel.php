0587f2a4167207c52fb62b04f40747681cf1609b
Haryo Kuncoro <61614504+haryo-sk@users.noreply.github.com>
2022-07-18 16:50:26 +0700

    Update FamilyModel.php

--- Models/FamilyModel.php
+++ Models/FamilyModel.php
@@ -8,5 +8,5 @@ class FamilyModel extends Model
 {
     protected $table = 'ci_data_keluarga';
     protected $primaryKey = 'ID';
-    protected $allowedFields = ['NAMA_LENGKAP','TGL_LAHIR','STATUS'];
+    protected $allowedFields = ['NAMA_LENGKAP','TGL_LAHIR','STATUS','KD_USER'];
 }
